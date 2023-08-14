16f3b6dfc7b1f0948be6a971125648b362af3ee3
SkepticalYouth <chamber2002@gmail.com>
2023-08-14 11:17:18 +0700

    create 16x16 board

--- static/logic/logic.js
+++ static/logic/logic.js
@@ -12,11 +12,30 @@ document.body.appendChild(flexbox)
 const grid = document.createElement('div')
 grid.classList.add("grid")
 gridStyle = grid.style
+gridStyle.display = "flex"
 gridStyle.width = "480px"
 gridStyle.height = "480px"
 gridStyle.backgroundColor = "white"
-gridStyle.border="solid 1px"
+gridStyle.borderCollapse = 'collapse'
+gridStyle.border="solid 1px" 
+gridStyle.overflow = "hidden"
+gridStyle.flexDirection = "row"
+gridStyle.flexWrap = 'wrap'
 flexbox.appendChild(grid)
+
+//Styling the inside of .grid
+let i = 0
+while (i<256){
+    const miniBox = document.createElement('div')
+    miniBoxStyle = miniBox.style
+    miniBoxStyle.width = '30px'
+    miniBoxStyle.height = '30px'
+    miniBoxStyle.border = 'none'
+    miniBox.classList.add('etchBlock')
+    // miniBoxStyle.borderCollapse = 'collapse'
+    grid.appendChild(miniBox)
+    i +=1  
+}
 
 
 
@@ -33,22 +52,26 @@ document.body.appendChild(buttonMenu)
 //Buttons
 const togglePen = document.createElement('button')
 togglePen.innerText="Toggle pen"
-togglePen.classList.add("common")
+// togglePen.classList.add("common")
+togglePen.id= 'pen'
 togglePen.style.margin="10px"
 
 const toggleColor = document.createElement('button')
 toggleColor.innerText = 'Toggle color'
-toggleColor.classList.add("common")
+// toggleColor.classList.add("common")
+toggleColor.id = 'color'
 toggleColor.style.margin="10px"
 
 const eraser = document.createElement('button')
 eraser.innerText = 'Eraser'
-eraser.classList.add("common")
+// eraser.classList.add("common")
+eraser.id='eraser'
 eraser.style.margin="10px"
 
 const reset = document.createElement('button')
 reset.innerText = 'Reset'
-reset.classList.add("common")
+reset.id = 'reset'
+// reset.classList.add("common")
 reset.style.margin="10px"
 
 //Styling the buttons
@@ -58,3 +81,7 @@ reset.style.margin="10px"
 // });
 
 buttonMenu.append(togglePen, toggleColor, eraser, reset)
+
+function pen(){
+
+}
